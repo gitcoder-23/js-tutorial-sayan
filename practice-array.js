b482963854f5d269758c35a5e292e9333a4911bc
Sayan <sayand.capitalnumbers.com>
2023-08-08 19:02:22 +0530

    [new upload]

--- practice-array.js
+++ practice-array.js
@@ -1,6 +1,6 @@
 // Splice --> replace
 let fruit = ['mango', 'banana', 'lichi','cucumber','chiku'];
-let spliceRArr1 = fruit.splice(1, 2, 'jackfruit');
+//let spliceRArr1 = fruit.splice(1, 2, 'jackfruit');
 //console.log('splice-replace-array->', fruit);
 // Concat using spread operator
 let pSa = ['r', 2, 34];
@@ -13,9 +13,16 @@ let say1 = ['b', 'rr', 'yug', 'bom', 55, 10];
 
 console.log('say-array1->', say1.length);
 
-// let arr2 = [5];
-// let arr3 = [true, false];
-// let pushArr1 = arr2.push(2, 3, 4, ...arr3);
-// console.log('pushed-arr2->', arr2);
+let mess = [74];
+let ron = [true, false];
+let messron = mess.push(21, 35, 48, ...ron);
+console.log('pushed-messron->', mess);
 // console.log('pushed-pushArr1->', pushArr1);
 
+// Unshift add one or more elements to the beginning of an array.
+
+
+let uArr10 = [6, 88, 66];
+let arr40 = [true, false];
+let unshiftArr10 = uArr10.unshift(1, 2, ...arr40);
+console.log('unshift-Array', uArr10);
