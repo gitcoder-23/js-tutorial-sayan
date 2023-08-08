bf2e6f0e60a6b6a55d87393223c38c086b11edea
Sayan <sayand.capitalnumbers.com>
2023-08-08 17:53:57 +0530

    [new upload message]

---
+++ practice-array.js
@@ -0,0 +1,21 @@
+// Splice --> replace
+let fruit = ['mango', 'banana', 'lichi','cucumber','chiku'];
+let spliceRArr1 = fruit.splice(1, 2, 'jackfruit');
+//console.log('splice-replace-array->', fruit);
+// Concat using spread operator
+let pSa = ['r', 2, 34];
+let rSa = [6, 88, 74];
+let concatNew = [...pSa, ...rSa];
+//console.log('concatNew->', concatNew);
+
+// Array
+let say1 = ['b', 'rr', 'yug', 'bom', 55, 10];
+
+console.log('say-array1->', say1.length);
+
+// let arr2 = [5];
+// let arr3 = [true, false];
+// let pushArr1 = arr2.push(2, 3, 4, ...arr3);
+// console.log('pushed-arr2->', arr2);
+// console.log('pushed-pushArr1->', pushArr1);
+
